f1144d0014054cdb527cb4796c29269486ea109c
Daniel Stefanelli <stefanellid@media-saturn.com>
2025-07-13 17:11:41 +0200

    refactor: remove class

--- src/main/java/com/dstefanelli/springbootdemo/domain/model/player/InventoryItem.java
+++
@@ -1,9 +0,0 @@
-package com.dstefanelli.springbootdemo.domain.model.player;
-
-import lombok.Data;
-
-@Data
-public class InventoryItem {
-    private String itemCode;
-    private Integer quantity;
-}
