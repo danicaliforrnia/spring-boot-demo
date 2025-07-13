656b089627827f703ebbe2b738e854e935fcbd94
Daniel Stefanelli <stefanellid@media-saturn.com>
2025-07-13 16:29:23 +0200

    feat: create private methods

--- src/main/java/com/dstefanelli/springbootdemo/domain/model/PlayerCampaignMatcher.java
+++ src/main/java/com/dstefanelli/springbootdemo/domain/model/PlayerCampaignMatcher.java
@@ -1,18 +1,33 @@
 package com.dstefanelli.springbootdemo.domain.model;
 
+import java.util.List;
+import java.util.Map;
+
 public class PlayerCampaignMatcher {
     public static boolean matches(PlayerProfile profile, Campaign campaign) {
-        boolean levelMatch = profile.getLevel() >= campaign.getMatchers().getLevel().getMin()
-                && profile.getLevel() <= campaign.getMatchers().getLevel().getMax();
+        boolean levelMatch = PlayerCampaignMatcher.matchLevel(profile.getLevel(), campaign.getMatchers().getLevel());
+        boolean countryMatch = PlayerCampaignMatcher.matchCountries(profile.getCountryCode(), campaign.getMatchers().getHas().getCountry());
+        boolean hasItems = PlayerCampaignMatcher.hasItems(profile.getInventory(), campaign.getMatchers().getHas().getItems());
+        boolean doesNotHaveItems = PlayerCampaignMatcher.hasNoItems(profile.getInventory(), campaign.getMatchers().getDoesNotHave().getItems());
+        return campaign.getEnabled() && levelMatch && countryMatch && hasItems && doesNotHaveItems;
+    }
 
-        boolean countryMatch = campaign.getMatchers().getHas().getCountry().contains(profile.getCountryCode());
+    private static boolean matchLevel(Integer level, CampaignMatchers.Level campaignLevelRanges) {
+        return level >= campaignLevelRanges.getMin()
+                && level <= campaignLevelRanges.getMax();
+    }
 
-        boolean hasItems = campaign.getMatchers().getHas().getItems().stream()
-                .allMatch(item -> profile.getInventory().containsKey(item));
+    private static boolean matchCountries(String countryCode, List<String> countries) {
+        return countries.contains(countryCode);
+    }
 
-        boolean doesNotHaveItems = campaign.getMatchers().getDoesNotHave().getItems().stream()
-                .noneMatch(item -> profile.getInventory().containsKey(item));
+    private static boolean hasItems(Map<String, Integer> playerInventory, List<String> items) {
+        return items.stream()
+                .allMatch(playerInventory::containsKey);
+    }
 
-        return levelMatch && countryMatch && hasItems && doesNotHaveItems;
+    private static boolean hasNoItems(Map<String, Integer> playerInventory, List<String> items) {
+        return items.stream()
+                .noneMatch(playerInventory::containsKey);
     }
 }
