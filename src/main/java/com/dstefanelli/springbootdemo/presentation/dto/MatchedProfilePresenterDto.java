9226a48c9a625d4b884b016964ccd9f90057ecc0
Daniel Stefanelli <stefanellid@media-saturn.com>
2025-07-13 16:31:02 +0200

    feat: specify JsonNaming

--- src/main/java/com/dstefanelli/springbootdemo/presentation/dto/MatchedProfilePresenterDto.java
+++ src/main/java/com/dstefanelli/springbootdemo/presentation/dto/MatchedProfilePresenterDto.java
@@ -1,15 +1,18 @@
 package com.dstefanelli.springbootdemo.presentation.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record MatchedProfilePresenterDto(@JsonProperty("player_id") UUID id,
                                          Integer level,
                                          Integer xp,
                                          @JsonProperty("country") String countryCode,
                                          Map<String, Integer> inventory,
-                                         @JsonProperty("active_campaigns") List<String> activeCampaigns) {
+                                         List<String> activeCampaigns) {
 }
